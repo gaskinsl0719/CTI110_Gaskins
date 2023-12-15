69fb8eaf94eca0d4d15bc2b0353f26f15b8d9316
gaskinsl0719 <144837534+gaskinsl0719@users.noreply.github.com>
2023-12-14 20:57:41 -0500

    added p1lab1

---
+++ P1Lab1_Gaskins.py
@@ -0,0 +1,33 @@
+"""
+CTI 110 
+P1LAB!
+GaskinsL
+8/24/2023
+"""
+
+#Say hello to the user 
+print ("Hello! What's your name?")
+
+
+#get the user's name 
+name = input()
+
+print(name,"is a nice name!")
+
+#get the user's mood
+print ("How are you?")
+mood = input()
+
+print ("That's great!")
+#this is kind of messed up if they say "not good"
+
+#get the user's favorite book
+print ("What is your favorite book?")
+book = input()
+
+print(book,"is a cool book!")
+
+
+
+
+
